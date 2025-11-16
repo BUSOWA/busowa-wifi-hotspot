a67d3e49e411359d3d8060ee2a33c5514c87596e
BUSOWA <busowahotspot@gmail.com>
2025-11-16 17:07:00 +0300

    Create Packages.ts

---
+++ data/Packages.ts
@@ -0,0 +1,24 @@
+// data/packages.ts
+export const packages = [
+  {
+    id: 1,
+    title: "8 HOURS",
+    hours: 8,
+    price: 500,
+    unlimited: false,
+  },
+  {
+    id: 2,
+    title: "24 HOURS",
+    hours: 24,
+    price: 900,
+    unlimited: true,
+  },
+  {
+    id: 3,
+    title: "2 DAYS",
+    hours: 48,
+    price: 1500,
+    unlimited: true,
+  },
+];
