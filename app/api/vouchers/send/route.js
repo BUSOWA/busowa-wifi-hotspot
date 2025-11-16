00882db44b36ede100e60f43cb153f463ea64125
BUSOWA <busowahotspot@gmail.com>
2025-11-16 17:25:11 +0300

    Update route.js

--- app/api/vouchers/send/route.js
+++ app/api/vouchers/send/route.js
@@ -1,1 +1,9 @@
+// app/api/vouchers/send/route.js
+export async function POST(request) {
+  const { phone, code } = await request.json();
 
+  // TODO: Send via WhatsApp/SMS
+  console.log("Sending voucher to", phone, ":", code);
+
+  return Response.json({ sent: true });
+}
