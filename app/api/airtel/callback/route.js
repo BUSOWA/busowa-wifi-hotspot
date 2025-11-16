14e3a765d6539aad53ae14acc9878317badb2c5a
BUSOWA <busowahotspot@gmail.com>
2025-11-16 17:24:10 +0300

    Update route.js

--- app/api/airtel/callback/route.js
+++ app/api/airtel/callback/route.js
@@ -1,1 +1,9 @@
+// app/api/airtel/callback/route.js
+export async function POST(request) {
+  const body = await request.json();
+  console.log("Airtel Callback:", body);
 
+  // TODO: Validate payment, generate voucher, call /api/vouchers/send
+
+  return Response.json({ received: true });
+}
